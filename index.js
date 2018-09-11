e94e928c75ae1626ea862943555874f989fc068e
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-11 21:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if(name === "Susan") {
   } else {
     console.log("Expected the name to be Susan")
   }
-if(height ==== "74") {
+if(height === "74") {
   console.log("The height is correct")
 } else {
   console.log("Expected the height to be 74")
